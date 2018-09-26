dd0a24181c54ae7c071ef500eefb02d78076a025
Przemyslaw Cygan <cyganprzemyslaw@gmail.com>
2018-09-26 21:39:43 +0200

    Change handlers.js, Change server.js, Change index.html, Add style.css file

--- modules/handlers.js
+++ modules/handlers.js
@@ -1,11 +1,20 @@
 var fs = require('fs');
 var formidable = require('formidable');
+var fileName;
 
 exports.welcome = function(request, response) {
     console.log("Rozpoczynam obsługę żądania welcome.");
-    fs.readFile('templates/index.html', function(err, html) {
+    fs.readFile('templates/index.html', function(err, html) { 
         response.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
         response.write(html);
+        response.end();
+    });
+}
+exports.style = function(request, response) {
+    console.log("Rozpoczynam obsługę żądania style.");
+    fs.readFile('./style.css', function(err, css) { 
+        response.writeHead(200, {"Content-Type": "text/css"});
+        response.write(css);
         response.end();
     });
 }
@@ -14,7 +23,8 @@ exports.upload = function(request, response) {
     console.log("Rozpoczynam obsługę żądania upload.");
     var form = new formidable.IncomingForm();
     form.parse(request, function(error, fields, files) {
-        fs.renameSync(files.upload.path, "test.png");
+    	fileName = files.upload.name;
+        fs.renameSync(files.upload.path, fileName);
         response.writeHead(200, {"Content-Type": "text/html"});
         response.write("received image:<br/>");
         response.write("<img src='/show' />");
@@ -23,7 +33,7 @@ exports.upload = function(request, response) {
 }
 
 exports.show = function(request, response) {
-    fs.readFile("test.png", "binary", function(error, file) {
+    fs.readFile(fileName, "binary", function(error, file) {
         response.writeHead(200, {"Content-Type": "image/png"});
         response.write(file, "binary");
         response.end();
